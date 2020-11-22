7a36012ea14b4ab6c990b3489ec6e7f81ea6834f
sdflower <sdflower@coupang.com>
2020-11-22 23:47:26 +0900

    모듈 정리..

--- study-jdk14/src/sdflower80/study/record/Person.java
+++
@@ -1,9 +0,0 @@
-package sdflower80.study.record;
-
-public record Person(String name, Integer age) {
-
-    public static void main(String[] args) {
-        Person p = new Person("sdflower80", 44);
-        System.out.println(p);
-    }
-}
