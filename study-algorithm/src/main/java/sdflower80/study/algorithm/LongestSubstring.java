8fd454e13467e38fa974937acd7e70cbc70c89af
sdflower <sdflower@coupang.com>
2020-11-23 00:45:26 +0900

    알고리즘 연습

--- study-algorithm/src/main/java/sdflower80/study/algorithm/LongestSubstring.java
+++ study-algorithm/src/main/java/sdflower80/study/algorithm/LongestSubstring.java
@@ -1,6 +1,7 @@
 package sdflower80.study.algorithm;
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author sdflower
@@ -9,14 +10,13 @@ import java.util.Arrays;
 public class LongestSubstring {
 
     public static void main(String[] args) {
-        int pwwkew = new LongestSubstring().lengthOfLongestSubstring("pwwk");
-        System.out.println(pwwkew);
+        /*int pwwkew = new LongestSubstring().lengthOfLongestSubstring("pwwk");
+        System.out.println(pwwkew);*/
 
-        int[] index = new int[128];
-        System.out.println(Math.max(index["p".charAt(0)], 0));
+        System.out.println(new LongestSubstring().resolve2("pwwkew"));
     }
 
-    public int lengthOfLongestSubstring(String s) {
+    public int resolve1(String s) {
         int n = s.length(), ans = 0;
         int[] index = new int[128]; // current index of character
         // try to extend the range [i, j]
@@ -26,5 +26,24 @@ public class LongestSubstring {
             index[s.charAt(j)] = j + 1;
         }
         return ans;
+    }
+
+    // pww
+    public int resolve2(String s) {
+        int start = 0;
+        int longestLen = 0;
+        Set<String> index = new HashSet<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            String one = String.valueOf(s.charAt(i));
+            if (index.contains(one)) {
+                int curLen = s.substring(start, i).length();
+                longestLen = Math.max(longestLen, curLen);
+                start = i;
+            } else {
+                index.add(one);
+            }
+        }
+        return longestLen;
     }
 }
--- study-algorithm/src/main/java/sdflower80/study/algorithm/SumTwoList.java
+++ study-algorithm/src/main/java/sdflower80/study/algorithm/SumTwoList.java
@@ -1,7 +1,5 @@
 package sdflower80.study.algorithm;
 
-import java.util.Arrays;
-
 /**
  * @author sdflower
  * @since 12/06/2020 12:37
@@ -9,92 +7,44 @@ import java.util.Arrays;
 public class SumTwoList {
     public static void main(String[] args) {
         int[] one = new int[]{1, 2, 8};
-        int[] two = new int[]{2, 2, 1, 3, 4};
+        int[] two = new int[]{1, 1, 1, 1, 2};
 
-        // System.out.println(convertInt(two));
-        System.out.println(128%1000);
-    }
-
-    public static int[] resolve(int[] one, int[] two) {
-        return new int[]{};
-    }
-
-    public static int convertInt(int[] arr) {
-        int result = 0;
-        for (int i : arr) {
-            result = (result * 10) + i;
+        int[] result = resolve1(one, two);
+        for (int i : result) {
+            System.out.print(i);
         }
-        return result;
-    }
 
-    public static int[] convertArr(int num) {
-//        Arrays.copyOf()
-
-        int[] result = new int[10];
-        int moc = num;
-        while(true) {
-            int mod = moc % 10;
-            moc = num / 10;
-            if (moc < 1) {
-                break;
-            }
-        }
-        return new int[]{};
     }
 
     public static int[] resolve1(int[] one, int[] two) {
-        StringBuilder num1 = new StringBuilder();
-        for (int i : one) {
-            num1.append(i);
+
+        int a = convertArrToNum(one);
+        int b = convertArrToNum(two);
+
+        int total = a + b;
+
+        int[] result = new int[10];
+        int mok = total;
+        int idx = 0;
+        while(mok > 0) {
+            result[idx] = mok % 10;
+            mok = mok / 10;
+            idx ++;
         }
 
-        StringBuilder num2 = new StringBuilder();
-        for (int i : two) {
-            num2.append(i);
+        int[] result2 = new int[idx];
+        for(int i = (idx-1), j=0; i >= 0; i--, j++) {
+            result2[j] = result[i];
         }
 
-        String total = String.valueOf(Long.parseLong(num1.toString()) + Long.parseLong(num2.toString()));
-        char[] chars = total.toCharArray();
-        int[] result = new int[chars.length];
-        for (int i = 0; i < total.length(); i ++) {
-            result[i] = Character.getNumericValue(chars[i]);
-        }
-        return result;
+        return result2;
     }
 
-    public static int[] resolve2(int[] one, int[] two) {
-        int cnt = one.length > two.length ? one.length : two.length;
-
-        int diff = one.length - two.length;
-        diff = diff < 0 ? (-1) * diff : diff;
-
-        int[] total = new int[cnt];
-        int next_over = 0;
-        int curr_over = 0;
-        for(int i=cnt-1; i >= 0; i--) {
-            curr_over = next_over;
-
-            int a_idx = one.length < two.length ? i - diff : i;
-            int b_idx = one.length > two.length ? i - diff : i;
-
-            int a = a_idx < one.length && a_idx >= 0 ? one[a_idx] : 0;
-            int b = b_idx < two.length && b_idx >= 0 ? two[b_idx] : 0;
-            int c = a + b;
-            if (c >= 10) {
-                c -= 10;
-                next_over = 1;
-            } else {
-                next_over = 0;
-            }
-            total[i] = c + curr_over;
-            System.out.println(String.format("a=%s, b=%s, cur_over=%s, nex_over=%s, total=%s",
-                    a, b, curr_over, next_over, total[i]));
+    private static int convertArrToNum(int[] one) {
+        int a = 0;
+        for (int i = one.length - 1, d = 0; i >= 0; i--, d++) {
+            a += one[i] * (int)(Math.pow(10, d));
         }
-
-        if (next_over > 0) {
-            total[0] = next_over;
-        }
-
-        return total;
+        return a;
     }
 }
